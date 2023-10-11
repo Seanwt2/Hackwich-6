107be7e528df2703e00014fd718eda4eb79907e0
Sean W Taylor <142876161+Seanwt2@users.noreply.github.com>
2023-10-11 00:12:32 -1000

    Completed parts 1-8
    spent an hour on silly blunders:
    - deleted wrong brackets
    - repasted unnecessary line of code that sent an error message and cost me an hour of my life
    - connected wrong table to VC
    - Table cell delegate outlet wasn't connected properly...

--- Hackwich 6/Hackwich 6/ViewController.swift
+++ Hackwich 6/Hackwich 6/ViewController.swift
@@ -7,8 +7,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+   
+    //var
+    var myFriendsArray = ["Chris","Christina","Emma"]
+    
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+//   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//       func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//          return 1;
+//   }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+            return myFriendsArray.count
+     }
+
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+        {//*only needs one of these^ dont copy paste the whole thing again
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+                       let text = myFriendsArray[indexPath.row]
+                       cell.textLabel?.text = text
+                       return cell
+          }
+
+    
+   
+
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
